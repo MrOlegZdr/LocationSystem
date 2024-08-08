3bc2cf9b19354209dfc1b17ed7fdb2459b94136c
MrOleg <mrdrozdilla@gmail.com>
2024-08-08 17:18:30 +0200

    Initial commit

---
+++ src/test/java/com/home/project/locationsystem/LocationsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.project.locationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
